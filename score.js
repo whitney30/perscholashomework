381ac763ba1ec1dedc6137f55e0978c13f4d8ce0
whitney30 <87485304+whitney30@users.noreply.github.com>
2021-09-09 13:52:53 -0700

    Add files via upload

---
+++ score.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App";
+
+export default function Score(props) {
+    return (
+        <div>
+            <p>{props.score.date</p>
+            <p>{props.score.score</p>
+        </div>
+    );
+}
